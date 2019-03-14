209cb3044e305f3f38a9f52eb863e75cea52d477
Adam S <adam.sarli@gmail.com>
2019-03-14 18:50:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash.each do |season, holidays|
-    puts season.to_s.capitalize + ":"
+    puts season.to_s.capitalize! + ":"
     holidays.each do |holiday, supplies|
       holiday_string = "  " + holiday.to_s.capitalize + ":"
       if supplies.length == 2 
